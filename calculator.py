04b6caef67600c4a0d67449f8bbeabb124026005
z1rkt <xth3fearless@gmail.com>
2026-03-05 22:16:15 +0200

    feat: add calculator logic

---
+++ calculator.py
@@ -0,0 +1,18 @@
+class Calculator:
+
+    def add(self, a, b):
+        return a + b
+
+    def subtract(self, a, b):
+        return a - b
+
+    def multiply(self, a, b):
+        return a * b
+
+    def divide(self, a, b):
+        if b == 0:
+            raise ValueError("Division by zero is not allowed")
+        return a / b
+
+    def clear(self):
+        return 0
