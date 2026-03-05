8e068f222a43c37a0fa93da9c5215dfbea7c3e12
z1rkt <xth3fearless@gmail.com>
2026-03-05 22:27:02 +0200

    test: add integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -1,1 +1,9 @@
+from interface import calculate
 
+def test_full_operation():
+    result = calculate("+",5,3)
+    assert result == 8
+
+def test_clear_function():
+    result = calculate("C",0)
+    assert result == 0
